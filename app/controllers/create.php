c04465b59d9ab8f48e76001ca611a1e1ad94c34e
Jiamli-5050 <jiamli@algomau.ca>
2025-06-20 01:58:22 +0000

    added newUser() function to process new accounts

--- app/controllers/create.php
+++ app/controllers/create.php
@@ -4,5 +4,19 @@ class Create extends Controller {
 
     public function index() {		
 	    $this->view('create/index');
+
     }
+  public function newUser() {
+    $username = $_POST['username'] ??'';
+    $password = $_POST['password'] ??'';
+    $verifypassword = $_POST['verifypassword'] ??'';
+
+    $username = strtolower(trim($username));
+    $password = trim($password);
+    $verifypassword = trim($verifypassword);
+
+    $user = $this->model('User');
+    $user->createUser($username, $password, $verifypassword);
+  }
 }
+?>
